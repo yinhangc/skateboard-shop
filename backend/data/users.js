d9115deef77acece4ca798820802f889ee8aa072
Emily Cheng <yinhang265@gmail.com>
2021-09-01 19:27:07 +0800

    Database setup

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 8),
+    isAdmin: true
+  },
+  {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: bcrypt.hashSync('123456', 8),
+
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    password: bcrypt.hashSync('123456', 8),
+
+  },
+]
+
+export default users;
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import products from './data/products.js'
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const app = express();
 
 dotenv.config();
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+connectDB();
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id);
-  res.json(product);
-})
+app.use('/api/products', productRoutes)
+
+app.use(notFound); // Handle non-matching URL
+app.use(errorHandler); // Handle error from everywhere (e.g. database)
 
 const PORT = process.env.PORT || 5000;
 
