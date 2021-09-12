cf109d12e7c1bc247e0d0233ff17fd579e016cd3
Emily Cheng <yinhang265@gmail.com>
2021-09-12 20:25:08 +0800

    Prepare for 1st deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
@@ -15,6 +16,16 @@ connectDB();
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 app.use(notFound); // Handle non-matching URL
 app.use(errorHandler); // Handle error from everywhere (e.g. database)
